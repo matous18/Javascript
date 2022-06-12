3c2d439aeb8f93d2eeda23beef365a9e179c76ab
matous <alejandrotousa@gmail.com>
2022-06-11 19:40:04 -0500

    Adding arrays

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
 let nom = prompt("Ingrese su nombre");
 let corr = prompt("Ingrese su correo");
-let saldo = prompt("¿Cuánto dinero necesita?");
-let quot = prompt("¿Cuánto plazo necesita?");
 let age = prompt("Ingrese su edad");
-let int = 1.04;
 
-function calcular (saldo, quot, int) {
-    return saldo*int/quot
-}
+const listaSaldo = [];
+const listaCuota = [];
+let creditoN = prompt("¿Cuántos créditos desea?");
 
-let resultado = calcular(saldo,quot,int);
+do {
+    let saldo = prompt("¿Cuánto dinero necesita?");
+    listaSaldo.push (saldo);
+    console.log (listaSaldo.length);
 
-alert(resultado);
+    let quot = prompt("¿Cuánto plazo necesita?");
+    listaCuota.push (quot);
+    console.log (listaCuota.length);
+
+    let int = 1.04;
+
+    function calcular (saldo, quot, int) {
+        return saldo*int/quot
+    }
+
+    let listaResultado = [];
+    let resultado = calcular(saldo,quot,int);
+    listaResultado.push (resultado);
+    console.log (listaResultado.length)
+
+    alert(resultado);
+    
+} while (listaSaldo.length !=creditoN)
+
+alert ("Gracias!");
+
+
 
 
 
